866c5b9df1bdf9eecfc9863d4b9c52688b714e45
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-09-08 12:45:22 -0400

    2.8 List Tags

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -36,6 +36,45 @@ This is the third paragraph. Wrap each paragraph with the paragraph tag to tell 
                 </p>
 
             </div>
+            <div id="wd-lists">
+                <h4>List Tags</h4>
+                <h5>Ordered List Tag</h5>
+                How to make pancakes:
+                <ol id="wd-pancakes">
+                    <li>Mix dry ingredients.</li>
+                    <li>Add wet ingredients.</li>
+                    <li>Stir to combine.</li>
+                    <li>Heat a skillet or griddle.</li>
+                    <li>Pour batter onto the skillet.</li>
+                    <li>Cook until bubbly on top.</li>
+                    <li>Flip and cook the other side.</li>
+                    <li>Serve and enjoy!</li>
+                </ol>
+                My favorite recipe:
+                <ol id="wd-your-favorite-recipe">
+                    <li>Toast 2 Eggo Waffles</li>
+                    <li>Spread a generous amount of Peanut Butter on top of each toasted Eggo</li>
+                    <li>Drizzle Honey on both Eggos</li>
+                    <li>Season with cinnamon</li>
+                </ol>
+                <h5>Unordered List Tag</h5>
+                My favorite books (in no particular order)
+                <ul id="wd-my-books">
+                    <li>Dune</li>
+                    <li>Lord of the Rings</li>
+                    <li>Ender's Game</li>
+                    <li>Red Mars</li>
+                    <li>The Forever War</li>
+                </ul>
+                Your favorite books (in no particular order)
+                <ul id="wd-your-books">
+                    <li>Sula</li>
+                    <li>A Raisin in the Sun</li>
+                    <li>Finding Me: A Memoir: Davis, Viola</li>
+                    <li>Vanishing Half</li>
+                {/* complete on your own */}
+                </ul>
+            </div>
         </div>
     );
 }
