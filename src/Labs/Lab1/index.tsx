d94fd53568ae8fff927873e8199ba75cd3ec5d65
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-09-08 13:24:26 -0400

    2.11 Form Tags

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -167,7 +167,86 @@ This is the third paragraph. Wrap each paragraph with the paragraph tag to tell 
                 Loading a local image:
                 <br />
                 <img id="wd-teslabot" src="images/teslabot.jpg" height="200px" />
-                </div>
+            </div>
+            <div id="wd-forms">
+                <h4>Form Elements</h4>
+                <form id="wd-text-fields">
+                    <h5>Text Fields</h5>
+                    <label htmlFor="wd-text-fields-username">Username:</label>
+                    <input id="wd-text-fields-username" placeholder="mpatrick" /> <br />
+                    <label htmlFor="wd-text-fields-password">Password:</label>
+                    <input type="password" id="wd-text-fields-password" value="123@#$asd" />
+                    <br />
+                    <label htmlFor="wd-text-fields-first-name">First name:</label>
+                    <input type="text" id="wd-text-fields-first-name" value="Madisen" title="John" /> <br />
+                    <label htmlFor="wd-text-fields-last-name">Last name:</label>
+                    <input type="text" id="wd-text-fields-last-name" placeholder="Doe"
+                    value="Patrick" title="The last name" />
+                    {/* copy rest of form elements here */}
+                </form>
+
+                <h5>Text boxes</h5>
+                <label>Biography:</label><br/>
+                <textarea id="wd-textarea" cols={30} rows={10}>
+                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+                </textarea>
+                <h5 id="wd-buttons">Buttons</h5>
+                <button id="wd-all-good" onClick={() => alert("Life is Good!")} type="button">
+                    Hello World!
+                </button>
+                <h5>File upload</h5>
+                <input id="wd-upload" type="file"/>
+                <h5 id="wd-checkboxes">Checkboxes</h5>
+                <label>Favorite movie genre:</label><br/>
+                <input type="checkbox" name="check-genre" id="wd-chkbox-comedy"/>
+                <label htmlFor="wd-chkbox-comedy">Comedy</label><br/>
+                <input type="checkbox" name="check-genre" id="wd-chkbox-drama"/>
+                <label htmlFor="wd-chkbox-drama">Drama</label><br/>
+                <input type="checkbox" name="check-genre" id="wd-chkbox-scifi"/>
+                <label htmlFor="wd-chkbox-scifi">Science Fiction</label><br/>
+                <input type="checkbox" name="check-genre" id="wd-chkbox-fantasy"/>
+                <label htmlFor="wd-chkbox-fantasy">Fantasy</label>
+
+                <h4 id="wd-dropdowns">Dropdowns</h4>
+                <h5>Select one</h5>
+                <label htmlFor="wd-select-one-genre"> Favorite movie genre: </label><br/>
+                <select id="wd-select-one-genre">
+                    <option value="COMEDY">Comedy</option>
+                    <option value="DRAMA">Drama</option>
+                    <option selected value="SCIFI">
+                    Science Fiction</option>
+                    <option value="FANTASY">Fantasy</option>
+                </select>
+                <h5>Select many</h5>
+                <label htmlFor="wd-select-many-genre"> Favorite movie genres: </label><br/>
+                <select id="wd-select-many-genre" multiple>
+                    <option selected value="COMEDY">Comedy</option>
+                    <option value="DRAMA">Drama</option>
+                    <option selected value="SCIFI">
+                    Science Fiction</option>
+                    <option value="FANTASY">Fantasy</option>
+                </select>
+                <form> 
+                <h4>Other HTML field types</h4>
+                <label htmlFor="wd-text-fields-email"> Email: </label>
+                <input type="email" placeholder="jdoe@somewhere.com" id="wd-text-fields-email"/><br/>
+                <label htmlFor="wd-text-fields-salary-start"> Starting salary:
+                </label>
+                <input type="number"
+                id="wd-text-fields-salary-start"
+                placeholder="1000"
+                value="100000"/><br/>
+                <label htmlFor="wd-text-fields-rating"> Rating: </label>
+                <input type="range" id="wd-text-fields-rating"
+                placeholder="Doe"
+                max="5"
+                value="4"/><br/>
+                <label htmlFor="wd-text-fields-dob"> Date of birth: </label>
+                <input type="date"
+                id="wd-text-fields-dob"
+                value="2000-01-21"/><br/>
+                </form>
+            </div>
         </div>
     );
 }
