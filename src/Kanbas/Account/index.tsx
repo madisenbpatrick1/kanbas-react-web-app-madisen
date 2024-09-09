3983258c6e899720f4da091f8295dafa495dbfc2
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-09-08 22:01:53 -0400

    3.2.2 Implementing the Profile Screen

--- src/Kanbas/Account/index.tsx
+++ src/Kanbas/Account/index.tsx
@@ -1,9 +1,12 @@
 import Signin from "./Signin";
+import Profile from "./Profile";
+
 export default function Account() {
     return (
         <div id="wd-account-screen">
             <h2>Account</h2>
             <Signin />
+            <Profile />
         </div>
     );
 }
