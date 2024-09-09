5e1d36777ed1b308059520c1aed0cfc2ee99077f
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-09-08 22:08:32 -0400

    3.2.4 Account Screen Navigation

--- src/Kanbas/Account/index.tsx
+++ src/Kanbas/Account/index.tsx
@@ -1,3 +1,4 @@
+import { Routes, Route, Navigate } from "react-router";
 import Signin from "./Signin";
 import Profile from "./Profile";
 import Signup from "./Signup";
@@ -6,9 +7,12 @@ export default function Account() {
     return (
         <div id="wd-account-screen">
             <h2>Account</h2>
-            <Signin />
-            <Profile />
-            <Signup />
+            <Routes>
+                <Route path="/" element={<Navigate to="/Kanbas/Account/Signin" />} />
+                <Route path="/Signin" element={<Signin />} />
+                <Route path="/Profile" element={<Profile />} />
+                <Route path="/Signup" element={<Signup />} />
+            </Routes>
         </div>
     );
 }
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -5,7 +5,11 @@ export default function Kanbas() {
     return (
         <div id="wd-kanbas">
             <h1>Kanbas</h1>
-            <Account />
+            <Routes>
+                <Route path="/" element={<Navigate to="Account" />} />
+                <Route path="/Account/*" element={<Account />} />
+            </Routes>
+
         </div>
     )
 }
