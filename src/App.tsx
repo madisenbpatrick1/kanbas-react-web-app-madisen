2113675007da586aaab560f190802b0f5f84e3e0
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-09-08 21:43:14 -0400

    2.13 Implementing Navigation in Single Page Applications

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 import Labs from "./Labs";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <HashRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Navigate to="Labs" />} />
+          <Route path="/Labs/*" element={<Labs />}/>
+        </Routes>
       <Labs />
     </div>
+    </HashRouter>
   );
 }
 
