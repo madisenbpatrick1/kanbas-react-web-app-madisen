df193744562d49738ecd4e01ab55fc66003be905
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-09-08 22:10:26 -0400

    3.2.5 Account Navigation Sidebar

--- src/Kanbas/Account/index.tsx
+++ src/Kanbas/Account/index.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate } from "react-router";
+import AccountNavigation from "./Navigation";
 import Signin from "./Signin";
 import Profile from "./Profile";
 import Signup from "./Signup";
@@ -6,13 +7,21 @@ import Signup from "./Signup";
 export default function Account() {
     return (
         <div id="wd-account-screen">
-            <h2>Account</h2>
-            <Routes>
-                <Route path="/" element={<Navigate to="/Kanbas/Account/Signin" />} />
-                <Route path="/Signin" element={<Signin />} />
-                <Route path="/Profile" element={<Profile />} />
-                <Route path="/Signup" element={<Signup />} />
-            </Routes>
+            <table>
+                <tr>
+                    <td valign="top">
+                        <AccountNavigation />
+                    </td>
+                    <td valign="top">
+                        <Routes>
+                            <Route path="/" element={<Navigate to="/Kanbas/Account/Signin" />} />
+                            <Route path="/Signin" element={<Signin />} />
+                            <Route path="/Profile" element={<Profile />} />
+                            <Route path="/Signup" element={<Signup />} />
+                        </Routes>
+                    </td>
+                </tr>
+            </table>
         </div>
     );
 }
