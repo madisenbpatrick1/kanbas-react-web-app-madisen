6dc71df9962ce243295762152d70cf5d188e18fd
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-09-08 21:49:10 -0400

    3.1 Kanbas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Labs from "./Labs";
+import Kanbas from './Kanbas';
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="Labs" />} />
           <Route path="/Labs/*" element={<Labs />}/>
+          <Route path="/Kanbas/*" element={<Kanbas />}/>
         </Routes>
       <Labs />
     </div>
