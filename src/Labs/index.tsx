e80943c460890b80a78194fc62a791b984a0d5e0
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-09-17 18:43:06 -0400

    Last updates added Section number

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -9,10 +9,10 @@ export default function Labs() {
     return (
         <div id="wd-labs">
             <h1>Madisen Patrick</h1>
+            <h1>Section: 01 </h1>
             <h1>Labs</h1>
             <TOC />
             <Routes>
-                <Route path="/" element={<Navigate to="Lab1" />} />
                 <Route path="Lab1" element={<Lab1 />} />
                 <Route path="Lab2" element={<Lab2 />} />
                 <Route path="Lab3" element={<Lab3 />} />
