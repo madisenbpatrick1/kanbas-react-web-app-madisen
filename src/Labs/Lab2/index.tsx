7eb57a784af6433cab631d376724b9704c829885
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-09-19 18:14:21 -0400

    3.1.10 Styling margins and padding

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -75,6 +75,51 @@ export default function Lab2() {
                 <p className="wd-border-thin wd-border-blue wd-border-dashed">
                     Dashed thin blue border</p>
             </div>
+            <div id="wd-css-paddings">
+                <h2>Padding</h2>
+                <div className="wd-padded-top-left wd-border-fat
+      wd-border-red wd-border-solid
+      wd-bg-color-yellow">
+                    Padded top left
+                </div>
+                <div className="wd-padded-bottom-right wd-border-fat
+      wd-border-blue wd-border-solid
+      wd-bg-color-yellow">
+                    Padded bottom right
+                </div>
+                <div className="wd-padding-fat wd-border-fat
+      wd-border-yellow wd-border-solid
+      wd-bg-color-blue wd-fg-color-white">
+                    Padded all around
+                </div>
+            </div>
+            <div id="wd-css-margins">
+                <h2>Margins</h2>
+                <div
+                    className="wd-margin-bottom
+      wd-padded-top-left
+      wd-border-fat wd-border-red
+      wd-border-solid
+      wd-bg-color-yellow">
+                    Margin bottom
+                </div>
+                <div
+                    className="wd-margin-right-left
+      wd-padded-bottom-right
+      wd-border-fat wd-border-blue
+      wd-border-solid
+      wd-bg-color-yellow">
+                    Margin left right
+                </div> <div
+                    className="wd-margin-all-around
+      wd-padding-fat wd-border-fat
+      wd-border-yellow
+      wd-border-solid
+      wd-bg-color-blue
+      wd-fg-color-white">
+                    Margin all around
+                </div>
+            </div>
         </div>
     );
 }
