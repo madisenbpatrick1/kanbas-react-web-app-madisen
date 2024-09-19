dd335b440c147d32004558fff46bce75b5047913
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-09-19 18:10:13 -0400

    3.1.9 Styling borders

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -68,6 +68,13 @@ export default function Lab2() {
                         the background of this text is green and the foreground white
                     </span> </p>
             </div>
+            <div id="wd-css-borders">
+                <h2>Borders</h2>
+                <p className="wd-border-fat wd-border-red wd-border-solid">
+                    Solid fat red border</p>
+                <p className="wd-border-thin wd-border-blue wd-border-dashed">
+                    Dashed thin blue border</p>
+            </div>
         </div>
     );
 }
