f200bc8a108632ea0bbbe9c62d4de6ad9517198b
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-09-19 18:21:11 -0400

    3.1.11 Styling corners

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -77,48 +77,52 @@ export default function Lab2() {
             </div>
             <div id="wd-css-paddings">
                 <h2>Padding</h2>
-                <div className="wd-padded-top-left wd-border-fat
-      wd-border-red wd-border-solid
-      wd-bg-color-yellow">
+                <div className="wd-padded-top-left wd-border-fat wd-border-red wd-border-solid wd-bg-color-yellow">
                     Padded top left
                 </div>
-                <div className="wd-padded-bottom-right wd-border-fat
-      wd-border-blue wd-border-solid
-      wd-bg-color-yellow">
+                <div className="wd-padded-bottom-right wd-border-fat wd-border-blue wd-border-solid wd-bg-color-yellow">
                     Padded bottom right
                 </div>
-                <div className="wd-padding-fat wd-border-fat
-      wd-border-yellow wd-border-solid
-      wd-bg-color-blue wd-fg-color-white">
+                <div className="wd-padding-fat wd-border-fat wd-border-yellow wd-border-solid wd-bg-color-blue  wd-fg-color-white">
                     Padded all around
                 </div>
             </div>
             <div id="wd-css-margins">
                 <h2>Margins</h2>
                 <div
-                    className="wd-margin-bottom
-      wd-padded-top-left
-      wd-border-fat wd-border-red
-      wd-border-solid
-      wd-bg-color-yellow">
+                    className="wd-margin-bottom wd-padded-top-left wd-border-fat wd-border-red wd-border-solid wd-bg-color-yellow">
                     Margin bottom
                 </div>
                 <div
-                    className="wd-margin-right-left
-      wd-padded-bottom-right
-      wd-border-fat wd-border-blue
-      wd-border-solid
-      wd-bg-color-yellow">
+                    className="wd-margin-right-left wd-padded-bottom-right wd-border-fat wd-border-blue wd-border-solid wd-bg-color-yellow">
                     Margin left right
-                </div> <div
-                    className="wd-margin-all-around
-      wd-padding-fat wd-border-fat
-      wd-border-yellow
-      wd-border-solid
-      wd-bg-color-blue
-      wd-fg-color-white">
+                </div>
+                <div
+                    className="wd-margin-all-around wd-padding-fat wd-border-fat wd-border-yellow wd-border-solid wd-bg-color-blue wd-fg-color-white">
                     Margin all around
                 </div>
+            </div>
+            <div id="wd-css-borders">
+                <h3>Rounded corners</h3>
+                <p className="wd-rounded-corners-top wd-border-thin
+     wd-border-blue wd-border-solid wd-padding-fat">
+                    Rounded corners on the top
+                </p>
+                <p className="wd-rounded-corners-bottom
+     wd-border-thin wd-border-blue wd-border-solid
+     wd-padding-fat">
+                    Rounded corners at the bottom
+                </p>
+                <p className="wd-rounded-corners-all-around
+     wd-border-thin wd-border-blue wd-border-solid
+     wd-padding-fat">
+                    Rounded corners all around
+                </p>
+                <p className="wd-rounded-corners-inline
+     wd-border-thin wd-border-blue wd-border-solid
+     wd-padding-fat">
+                    Different rounded corners
+                </p>
             </div>
         </div>
     );
