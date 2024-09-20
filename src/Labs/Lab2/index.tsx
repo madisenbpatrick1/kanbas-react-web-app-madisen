f20749565c17e49b25f87c3731daeec63b7c06da
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-09-20 12:01:54 -0400

    3.3.5 Hiding and showing responsive content

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,5 +1,6 @@
 import "./index.css";
 import ReactIconsSampler from "./ReactIcons";
+import ScreenSizeLabel from "./ScreenSizeLabel";
 export default function Lab2() {
     return (
         <div className="container">
@@ -357,6 +358,7 @@ export default function Lab2() {
                     </div>
                 </div>
             </div>
+            <ScreenSizeLabel />
         </div>
     );
 }
