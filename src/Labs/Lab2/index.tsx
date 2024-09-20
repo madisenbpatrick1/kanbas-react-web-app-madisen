2c72d6fa19d0ae1bf0579aacd3bb249183866d23
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-09-20 12:46:41 -0400

    3.3.16 Navigating with Tabs

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -562,6 +562,23 @@ export default function Lab2() {
                         Sign in </button>
                 </form>
             </div>
+            <div id="wd-css-navigating-with-tabs">
+                <h2>Tabs</h2>
+                <ul className="nav nav-tabs">
+                    <li className="nav-item">
+                        <a className="nav-link active" href="#">Active</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link disabled" href="#">Disabled</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 }
