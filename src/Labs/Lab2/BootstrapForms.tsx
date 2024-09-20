7a3d3c0969f396a758247ded12c30eeca672de34
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-09-20 12:43:27 -0400

    3.3.14 Styling Addons

--- src/Labs/Lab2/BootstrapForms.tsx
+++ src/Labs/Lab2/BootstrapForms.tsx
@@ -62,4 +62,17 @@
         <input type="range" className="form-range"
             min="0" max="5" step="0.5" id="range1" />
     </div>
+    <div id="wd-css-styling-addons">
+        <h3>Addons</h3>
+        <div className="input-group mb-3">
+            <span className="input-group-text">$</span>
+            <span className="input-group-text">0.00</span>
+            <input type="text" className="form-control" />
+        </div>
+        <div className="input-group">
+            <input type="text" className="form-control" />
+            <span className="input-group-text">$</span>
+            <span className="input-group-text">0.00</span>
+        </div>
+    </div>
 </div>
