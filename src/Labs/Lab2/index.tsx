169c2e59ba66e21f4034f2edbd4467bbc4f30277
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-09-20 12:42:52 -0400

    3.3.13 Styling Range and Sliders

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -460,6 +460,14 @@ export default function Lab2() {
                     </label>
                 </div>
             </div>
+            <div id="wd-css-styling-range-and-sliders">
+                <h3>Range</h3>
+                <label htmlFor="range1" className="form-label">
+                    Example range
+                </label>
+                <input type="range" className="form-range"
+                    min="0" max="5" step="0.5" id="range1" />
+            </div>
         </div>
     );
 }
