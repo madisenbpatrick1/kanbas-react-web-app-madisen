5ec4dc1a87f6e8ec7cc082eb252589173c02408e
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-09-20 12:40:22 -0400

    3.3.10 Basic Form Styling

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -405,6 +405,21 @@ export default function Lab2() {
                         className="list-group-item list-group-item-action disabled">
                         Ender's Game</a> </div>
             </div>
+            <div id="wd-css-styling-forms">
+                <h2>Forms</h2>
+                <div className="mb-3">
+                    <label htmlFor="input1" className="form-label">
+                        Email address</label>
+                    <input type="email" className="form-control"
+                        id="input1" placeholder="name@example.com" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="textarea1" className="form-label">
+                        Example textarea</label>
+                    <textarea className="form-control" id="textarea1"
+                        rows={3}></textarea>
+                </div>
+            </div>
         </div>
     );
 }
