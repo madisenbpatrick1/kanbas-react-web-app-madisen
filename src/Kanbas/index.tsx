a3079b71c6625f9b61bad9440da7b2b852d682d7
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-09-23 16:57:37 -0400

    4.1 Styling the Kanbas Navigation Sidebar

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -3,14 +3,15 @@ import Account from "./Account"
 import Dashboard from "./Dashboard"
 import KanbasNavigation from "./Navigation"
 import Courses from "./Courses"
+import "./styles.css";
 
 export default function Kanbas() {
     return (
         <div id="wd-kanbas">
             <KanbasNavigation />
-            <div>
+            <div className="wd-main-content-offset p-3">
                 <Routes>
-                    <Route path="/" element={<Navigate to="/Kanbas/Account" />} />
+                    <Route path="/" element={<Navigate to="/Kanbas/Dashboard" />} />
                     <Route path="/Account/*" element={<Account />} />
                     <Route path="/Dashboard" element={<Dashboard />} />
                     <Route path="/Courses/:cid/*" element={<Courses />} />
