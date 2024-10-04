33cc5e80f000bb7d1ddfc76a92b6e97b1aed9c62
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-10-04 11:46:41 -0400

    a3_2.2.2 Variable Types

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,8 +1,10 @@
 import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
 export default function Lab3() {
     return (
       <div id="wd-lab3">
         <h3>Lab 3</h3>
         <VariablesAndConstants />
+        <VariableTypes/>
   </div>
   ); }
