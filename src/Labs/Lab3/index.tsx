594b801b3e77086f56d9d79c86005d4a951d701b
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-10-04 11:51:59 -0400

    a3_2.2.3 Boolean Variables

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,10 +1,13 @@
+import BooleanVariables from "./BooleanVariables";
 import VariablesAndConstants from "./VariablesAndConstants";
 import VariableTypes from "./VariableTypes";
 export default function Lab3() {
     return (
-      <div id="wd-lab3">
-        <h3>Lab 3</h3>
-        <VariablesAndConstants />
-        <VariableTypes/>
-  </div>
-  ); }
+        <div id="wd-lab3">
+            <h3>Lab 3</h3>
+            <VariablesAndConstants />
+            <VariableTypes />
+            <BooleanVariables />
+        </div>
+    );
+}
