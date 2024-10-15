d05893c04810392971d9646d6e3d304795148922
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-10-15 16:44:59 -0400

    2.6.1 Child Components

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -27,6 +27,8 @@ import DestructingImports from "./DestructingImports";
 import Classes from "./Classes";
 import Styles from "./Styles";
 import Add from "./Add";
+import Square from "./Square";
+import Highlight from "./Highlight";
 
 
 export default function Lab3() {
@@ -40,7 +42,7 @@ export default function Lab3() {
             <TernaryOperator />
             <ConditionalOutputIfElse />
             {/* <hr/> */}
-            <ConditionalOutputInline/>
+            <ConditionalOutputInline />
             <LegacyFunctions />
             <ArrowFunctions />
             <ImpliedReturn />
@@ -62,7 +64,14 @@ export default function Lab3() {
             <DestructingImports />
             <Classes />
             <Styles />
-            <Add a={3} b={4}/>
+            <Add a={3} b={4} />
+            <h4>Square of 4</h4>
+            <Square>4</Square>
+            <br/>
+            <Highlight>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+                vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+            </Highlight>
         </div>
     );
 }
