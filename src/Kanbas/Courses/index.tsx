bb45eb7366652a9210e3e4a33cf0d475c6bc89b0
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-10-15 18:01:52 -0400

    a3_3.4 Data Driven Course Screen

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,17 +1,21 @@
 import CoursesNavigation from "./Navigation";
-import { Route, Routes } from "react-router";
+import { Route, Routes, useParams } from "react-router";
 import Modules from "./Modules";
 import Home from "./Home";
 import Assignment from "./Assignments/index";
 import AssignmentEditor from "./Assignments/Editor";
 import PeopleTable from "./People/Table";
 import { FaAlignJustify } from "react-icons/fa";
+import {courses } from "../Database";
+
 export default function Courses() {
+    const { cid } = useParams();
+    const course = courses.find((course) => course._id === cid);
     return (
         <div id="wd-courses">
             <h2 className="text-danger">
                 <FaAlignJustify className="me-4 fs-4 mb-1" />
-                Course 1234
+                {course && course.name}
             </h2>
             <hr />
             <div className="d-flex">
