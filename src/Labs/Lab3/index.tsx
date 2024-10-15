35bca7fe0ccac1c6ee3b94fd20b0173c406eb275
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-10-15 17:25:05 -0400

    2.7.1 Writing to the Console from JavaScript

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -33,6 +33,7 @@ import PathParameters from "./PathParameters";
 import AddPathParameters from "./AddPathParameters";
 
 export default function Lab3() {
+    console.log('Hello World!');
     return (
         <div id="wd-lab3">
             <h3>Lab 3</h3>
