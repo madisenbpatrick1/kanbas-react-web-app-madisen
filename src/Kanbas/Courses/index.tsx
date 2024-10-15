ba68cbc0d67839bac1e37ef094558521a3c7dfde
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-10-15 19:31:20 -0400

    3.6 Implementing the Breadcrumb Component

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,5 +1,5 @@
 import CoursesNavigation from "./Navigation";
-import { Route, Routes, useParams } from "react-router";
+import { Route, Routes, useParams, useLocation} from "react-router";
 import Modules from "./Modules";
 import Home from "./Home";
 import Assignment from "./Assignments/index";
@@ -11,11 +11,12 @@ import {courses } from "../Database";
 export default function Courses() {
     const { cid } = useParams();
     const course = courses.find((course) => course._id === cid);
+    const {pathname} = useLocation();
     return (
         <div id="wd-courses">
             <h2 className="text-danger">
                 <FaAlignJustify className="me-4 fs-4 mb-1" />
-                {course && course.name}
+                {course && course.name} &gt; {pathname.split("/")[4]}
             </h2>
             <hr />
             <div className="d-flex">
