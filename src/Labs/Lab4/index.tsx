541f25ed23c2862e65bcf888c3cb752cb3b04c46
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-10-21 10:55:28 -0400

    a4_2.2.3 Passing Functions as Parameters

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,11 +1,18 @@
 import ClickEvent from "./ClickEvent"
 import PassingDataOnEvent from "./PassingDataOnEvent"
+import PassingFunctions from "./PassingFunctions"
+
+
 export default function Lab4() {
+    function sayHello() {
+        alert("Hello");
+    }
     return(
         <div>
             <h1>Lab 4</h1> <hr />
             <ClickEvent />
             <PassingDataOnEvent />
+            <PassingFunctions theFunction={sayHello}/>
         </div>
     )
 }
