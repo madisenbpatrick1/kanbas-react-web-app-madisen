a792eda20205254925640371be04ebf04f5919ea
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-10-21 10:49:38 -0400

    a4_2.2.1 Handling Click Events

---
+++ src/Labs/Lab4/index.tsx
@@ -0,0 +1,9 @@
+import ClickEvent from "./ClickEvent"
+export default function Lab4() {
+    return(
+        <div>
+            <h1>Lab 4</h1> <hr />
+            <ClickEvent />
+        </div>
+    )
+}
