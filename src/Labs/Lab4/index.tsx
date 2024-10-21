956a6d4c696efe655188b8a470b825f27e7b5721
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-10-21 11:13:08 -0400

    a4_2.3.6 Object State Variables

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -6,6 +6,7 @@ import Counter from "./Counter";
 import BooleanStateVariables from "./BooLeanStateVariables";
 import StringStateVariables from "./StringStateVariables";
 import DateStateVariable from "./DateStateVariables";
+import ObjectStateVariable from "./ObjectStateVariable";
 
 export default function Lab4() {
     function sayHello() {
@@ -22,6 +23,7 @@ export default function Lab4() {
             <BooleanStateVariables />
             <StringStateVariables />
             <DateStateVariable />
+            <ObjectStateVariable />
         </div>
     )
 }
