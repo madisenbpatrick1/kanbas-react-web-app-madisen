91dca6d702a8072c9db9bde20a180896410ff779
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-10-21 10:53:06 -0400

    a4_2.2.2 Passing Data when Handling Events

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,9 +1,11 @@
 import ClickEvent from "./ClickEvent"
+import PassingDataOnEvent from "./PassingDataOnEvent"
 export default function Lab4() {
     return(
         <div>
             <h1>Lab 4</h1> <hr />
             <ClickEvent />
+            <PassingDataOnEvent />
         </div>
     )
 }
