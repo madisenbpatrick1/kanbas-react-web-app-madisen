9efbd2fd1b3c6f66c9d855c3589d22a95e7c8fe4
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-10-22 16:53:01 -0400

    a4_3.3.2 Deleting a Module

--- src/Kanbas/Courses/Modules/ModuleControlButtons.tsx
+++ src/Kanbas/Courses/Modules/ModuleControlButtons.tsx
@@ -1,11 +1,19 @@
 import { FaPlus } from "react-icons/fa6";
 import GreenCheckmark from "./GreenCheckmark";
+import { FaTrash } from "react-icons/fa";
+import { IoEllipsisVertical } from "react-icons/io5";
 
-export default function ModuleControlButtons(){
+export default function ModuleControlButtons(
+    { moduleId, deleteModule }: {
+        moduleId: string;
+        deleteModule: (moduleId: string) => void;
+    }) {
     return (
         <div className="float-end">
+            <FaTrash className="text-danger me-2 mb-1" onClick={() => deleteModule(moduleId)} />
             <GreenCheckmark />
             <FaPlus className="position-relative me-2" />
+            <IoEllipsisVertical className="fs-4" />
         </div>
     )
 }
