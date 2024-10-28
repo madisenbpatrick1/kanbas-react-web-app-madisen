41e2320754d3e77c0caf8432e9ab584f28614250
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-10-28 18:56:41 -0400

    Implemented Quizzes Controls

---
+++ src/Kanbas/Courses/Quizzes/QuizzesControls.tsx
@@ -0,0 +1,35 @@
+import { FaPlus } from "react-icons/fa6";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { useParams } from "react-router";
+import { IoEllipsisVertical } from "react-icons/io5";
+
+export default function QuizzesControls() {
+    const { cid } = useParams();
+    return (
+        <div id="wd-assignment-controls" className="text-nowrap">
+            <button id="wd-add-quizzes-btn" className="btn btn-lg btn-secondary me-1 float-end">
+                <IoEllipsisVertical className="fs-4" />
+            </button>
+            <Link to={`/Kanbas/Courses/${cid}/Quizzes/Editor`}>
+                <button
+                    id="wd-add-assignment-btn"
+                    className="btn btn-lg btn-danger me-1 float-end"
+                >
+                    <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
+                    Quiz
+                </button>
+            </Link>
+
+
+            <div className="d-inline me-1 float-left">
+                <button id="wd-search-quizzes-btn" className="btn btn-lg d-inline btn-secondary">
+                    <FaSearch className="position-relative me-2" style={{ bottom: "1px" }} />
+                    <input id="wd-search-quizzes"
+                        className="btn me-1 "
+                        placeholder="Search for Quiz..." />
+                </button>
+            </div>
+        </div>
+    );
+}
--- src/Kanbas/Courses/Quizzes/index.tsx
+++ src/Kanbas/Courses/Quizzes/index.tsx
@@ -1,9 +1,10 @@
+import QuizzesControls from "./QuizzesControls"
 
 
 export default function Quizzes() {
     return (
-        <div className="d-flex" id="wd-home">
-           <h1> Quizzes</h1>
+        <div id="wd-assignments">
+            <QuizzesControls/>
         </div>
 
     );
