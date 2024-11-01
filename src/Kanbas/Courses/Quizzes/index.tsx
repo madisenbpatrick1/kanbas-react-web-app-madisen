5a41703201f2ef7a080a6d36953d41fe0bc16b9a
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-11-01 17:13:09 -0400

    Initial set up of UI for Quiz Screen pt 2

--- src/Kanbas/Courses/Quizzes/index.tsx
+++ src/Kanbas/Courses/Quizzes/index.tsx
@@ -1,11 +1,56 @@
-import QuizzesControls from "./QuizzesControls"
-
+import { BsGripVertical } from "react-icons/bs";
+import QuizzesControls from "./QuizzesControls";
+import { useState } from "react";
+import * as db from "../../Database";
+import { useParams } from "react-router";
+import { RxRocket } from "react-icons/rx";
+import { FcCancel } from "react-icons/fc";
+import { IoEllipsisVertical } from "react-icons/io5";
+import GreenCheckmark from "../Modules/GreenCheckmark";
 
 export default function Quizzes() {
+    const [quizzes, setQuizzes] = useState("");
+    const { cid } = useParams();
+    const quizList = db.quizzes;
     return (
         <div id="wd-assignments">
-            <QuizzesControls/>
-        </div>
+            <QuizzesControls />
+            <hr />
+            <div id="wd-assignments">
+                <ul id="wd-assignment-list" className="list-group rounded-0 w-100" >
+                    <li className="wd-assignment-list-item list-group-item p-0 mb-5 fs-5 border-gray">
+                        <div className="wd-assignments-title p-3 ps-2 bg-secondary" >
+                            <BsGripVertical className="me-2 fs-3" />
+                            Assignment Quizzes
+                        </div>
+                        {quizList
+                            .filter((quiz: any) => quiz.course === cid).map((quiz: any) => (
+                                <ul className="wd-assignments-lessons list-group rounded-0">
+                                    <li className="wd-lesson list-group-item p-3 ps-1">
+                                        <div className="wd-grid-col-left-sidebar">
+                                            <RxRocket className="ms-2 fs-3" />
+                                        </div>
+                                        <div className="wd-grid-col-main-content">
+                                            <h3> {quiz.title}</h3>
+                                            <p>
+                                                <span > {quiz.availability} </span>
+                                                | Due {quiz.due_date} | {quiz.points} pts | {quiz.num_of_q} Questions
+                                            </p>
+                                        </div>
+                                        <div className="wd-grid-col-right-sidebar p-3 me-0">
+                                            <div className="float-end">
+                                                <FcCancel />
+                                                <GreenCheckmark />
+                                                <IoEllipsisVertical />
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            ))}
+                    </li>
+                </ul>
+            </div >
+        </div >
 
     );
 }
