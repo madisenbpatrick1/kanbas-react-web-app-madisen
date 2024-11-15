d35a0a0429f72514508d825f77356e74d27e7efe
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-11-15 12:45:39 -0500

    a5_3.6.1 Posting Data to Servers with HTTP POST Requests

--- src/Labs/Lab5/WorkingWithArraysAsynchronously.tsx
+++ src/Labs/Lab5/WorkingWithArraysAsynchronously.tsx
@@ -18,6 +18,10 @@ export default function WorkingWithArraysAsynchronously() {
         const updatedTodos = await client.removeTodo(todo);
         setTodos(updatedTodos);
     };
+    const postTodo = async () => {
+        const newTodo = await client.postTodo({ title: "New Posted Todo", completed: false, });
+        setTodos([...todos, newTodo]);
+    };
 
     useEffect(() => {
         fetchTodos();
@@ -28,6 +32,7 @@ export default function WorkingWithArraysAsynchronously() {
             <h4>Todos
                 <FaPlusCircle onClick={createTodo} className="text-success float-end fs-3"
                     id="wd-create-todo" />
+                <FaPlusCircle onClick={postTodo} className="text-primary float-end fs-3 me-3" id="wd-post-todo" />
             </h4>
             <ul className="list-group">
                 {todos.map((todo) => (
