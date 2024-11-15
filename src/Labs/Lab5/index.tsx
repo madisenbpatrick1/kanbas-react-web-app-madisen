0dd287d9618e97616c6bc47bd3f94b1f834ed35b
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-11-15 12:39:16 -0500

    a5_3.5.6 Working with Remote Arrays on a Server Asynchronously

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -3,6 +3,7 @@ import HttpClient from "./HTTPClient";
 import PathParameters from "./PathParameters";
 import QueryParameters from "./QueryParameters";
 import WorkingWithArrays from "./WorkingWithArrays";
+import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsynchronously";
 import WorkingWithObjects from "./WorkingWithObjects";
 import WorkingWithObjectsAsynchronously from "./WorkingWithObjectsAsynchronously";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
@@ -44,6 +45,7 @@ export default function Lab5() {
       <WorkingWithArrays />
       <HttpClient />
       <WorkingWithObjectsAsynchronously />
+      <WorkingWithArraysAsynchronously />
     </div>
   );
 }
