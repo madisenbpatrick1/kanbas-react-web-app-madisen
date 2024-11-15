7c285e39897fe2a3f8a0be3f1c9f3b8588e473e5
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-11-15 00:30:33 -0500

    a5_3.3.4

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -21,7 +21,22 @@ export default function Lab5() {
       <PathParameters />
       <QueryParameters />
       <WorkingWithObjects />
-      
+      <div className="list-group">
+        <a
+          href={`${REMOTE_SERVER}/lab5/module`}
+          className="list-group-item"
+        >
+          Get Module
+        </a>
+      </div>
+      <div className="list-group">
+        <a
+          href={`${REMOTE_SERVER}/lab5/module/name`}
+          className="list-group-item"
+        >
+          Get Module Name
+        </a>
+      </div>
     </div>
   );
 }
