39f129aadf9b98d3bc7d1422a7c83d6fc9d97602
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-11-15 00:41:28 -0500

    a5_3.4.6 Updating Data on a Server

--- src/Labs/Lab5/WorkingWithArrays.tsx
+++ src/Labs/Lab5/WorkingWithArrays.tsx
@@ -2,7 +2,13 @@ import React, { useState } from "react";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export default function WorkingWithArrays() {
     const API = `${REMOTE_SERVER}/lab5/todos`;
-    const [todo, setTodo] = useState({ id: "1" });
+    const [todo, setTodo] = useState({
+        id: "1",
+        title: "NodeJS Assignment",
+        description: "Create a NodeJS server with ExpressJS",
+        due: "2021-09-09",
+        completed: false,
+    });
 
 
     return (
@@ -32,6 +38,14 @@ export default function WorkingWithArrays() {
             <a id="wd-retrieve-completed-todos" className="btn btn-primary float-end" href={`${API}/${todo.id}/delete`}>
                 Delete Todo with ID = {todo.id} </a>
             <input value={todo.id} className="form-control w-50" onChange={(e) => setTodo({ ...todo, id: e.target.value })} /><hr />
+            <h3>Updating an Item in an Array</h3>
+            <a href={`${API}/${todo.id}/title/${todo.title}`} className="btn btn-primary float-end">
+                Update Todo</a>
+            <input value={todo.id} className="form-control w-25 float-start me-2"
+                onChange={(e) => setTodo({ ...todo, id: e.target.value })} />
+            <input value={todo.title} className="form-control w-50 float-start"
+                onChange={(e) => setTodo({ ...todo, title: e.target.value })} />
+            <br /><br /><hr />
 
         </div>
     );
