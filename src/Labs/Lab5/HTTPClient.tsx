bde9bc9e35105e7c8611ecf328adac4b671a91a9
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-11-15 12:33:38 -0500

    a5_3.5.4 Retrieving Data from a Server on Component Load

--- src/Labs/Lab5/HTTPClient.tsx
+++ src/Labs/Lab5/HTTPClient.tsx
@@ -1,17 +1,24 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import * as client from "./client";
 
-const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
+// const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 
 export default function HttpClient() {
     const [welcomeOnClick, setWelcomeOnClick] = useState("");
+    const [welcomeOnLoad, setWelcomeOnLoad] = useState("");
 
     const fetchWelcomeOnClick = async () => {
         const message = await client.fetchWelcomeMessage();
-
         setWelcomeOnClick(message);
     };
+    const fetchWelcomeOnLoad = async () => {
+        const welcome = await client.fetchWelcomeMessage();
+        setWelcomeOnLoad(welcome);
+    };
+    useEffect(() => {
+        fetchWelcomeOnLoad();
+    }, []);
+
 
     return (
         <div>
@@ -20,7 +27,11 @@ export default function HttpClient() {
             <button className="btn btn-primary me-2" onClick={fetchWelcomeOnClick}>
                 Fetch Welcome
             </button> <br />
+            <hr/>
             Response from server: <b>{welcomeOnClick}</b>
+            <h4>Requesting on Load</h4>
+            Response from server: <b>{welcomeOnLoad}</b>
+            <hr />
         </div>
     );
 }
