a9f8d36e64b130d06b5c87d6daa2a475d19d08d3
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-11-15 00:33:16 -0500

    a5_3.4.1 Retrieving Arrays from a Server

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -1,6 +1,7 @@
 import EnvironmentVariables from "./EnvironmentVariables";
 import PathParameters from "./PathParameters";
 import QueryParameters from "./QueryParameters";
+import WorkingWithArrays from "./WorkingWithArrays";
 import WorkingWithObjects from "./WorkingWithObjects";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export default function Lab5() {
@@ -37,6 +38,8 @@ export default function Lab5() {
           Get Module Name
         </a>
       </div>
+      <hr/>
+      <WorkingWithArrays />
     </div>
   );
 }
