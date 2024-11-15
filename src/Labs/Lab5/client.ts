933ff5d9aad4f8def96466d35157460b5e8ff337
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-11-15 12:40:43 -0500

    a5_3.5.7 Deleting Data from a Server Asynchronously

--- src/Labs/Lab5/client.ts
+++ src/Labs/Lab5/client.ts
@@ -21,3 +21,9 @@ export const fetchTodos = async () => {
   return response.data;
 };
 
+export const removeTodo = async (todo: any) => {
+    const response = await axios.get(`${TODOS_API}/${todo.id}/delete`);
+    return response.data;
+  };
+  
+  
