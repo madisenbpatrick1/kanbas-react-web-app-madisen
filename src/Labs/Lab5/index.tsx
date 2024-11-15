a95974cdc48743b88400dfce3fc8774ee1f2ec31
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-11-14 23:31:52 -0500

    a5_3.2.2 Sending Data to a Server with Query Parameters

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -1,5 +1,6 @@
 import EnvironmentVariables from "./EnvironmentVariables";
 import PathParameters from "./PathParameters";
+import QueryParameters from "./QueryParameters";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export default function Lab5() {
   console.log("remote",REMOTE_SERVER);
@@ -17,6 +18,7 @@ export default function Lab5() {
       <EnvironmentVariables />
       <hr />
       <PathParameters />
+      <QueryParameters />
       
     </div>
   );
