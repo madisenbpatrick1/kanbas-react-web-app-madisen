d337bded6e20f60c97db993faa8a224decc6f43a
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-11-17 18:47:47 -0500

    a5_4.2.3 Integrating the React Profile Screen with a RESTful Web API

--- src/Kanbas/Account/Profile.tsx
+++ src/Kanbas/Account/Profile.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { setCurrentUser } from "./reducer";
+import * as client from "./client";
 
 export default function Profile() {
     const [profile, setProfile] = useState<any>({});
@@ -16,6 +17,12 @@ export default function Profile() {
         dispatch(setCurrentUser(null));
         navigate("/Kanbas/Account/Signin");
     };
+
+    const updateProfile = async () => {
+        const updatedProfile = await client.updateUser(profile);
+        dispatch(setCurrentUser(updatedProfile));
+    };
+
     // eslint-disable-next-line
     useEffect(() => { fetchProfile(); }, []);
 
@@ -70,9 +77,13 @@ export default function Profile() {
                         <option value="FACULTY">Faculty</option>
                         <option value="STUDENT">Student</option>
                     </select>
-                    <button onClick={signout} className="btn btn-danger w-100 mb-2" id="wd-signout-btn">
-                        Sign out
-                    </button>
+                    <div>
+                        <button onClick={updateProfile} className="btn btn-primary w-100 mb-2"> Update </button>
+
+                        <button onClick={signout} className="btn btn-danger w-100 mb-2" id="wd-signout-btn">
+                            Sign out
+                        </button>
+                    </div>
                 </div>
             )}
         </div>
--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+
 export const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export const USERS_API = `${REMOTE_SERVER}/api/users`;
 
@@ -10,7 +11,12 @@ export const signin = async (credentials: any) => {
 export const signup = async (user: any) => {
     const response = await axios.post(`${USERS_API}/signup`, user);
     return response.data;
-  };
+};
 
-  
+export const updateUser = async (user: any) => {
+    const response = await axios.put(`${USERS_API}/${user._id}`, user);
+    return response.data;
+};
+
+
 
