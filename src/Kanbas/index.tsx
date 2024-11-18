1e02a09904b58cd0966836909edeb6880d1fc4a9
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-11-17 19:45:32 -0500

    a5_4.4.4 Updating a Course

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -42,7 +42,8 @@ export default function Kanbas() {
         const status = await courseClient.deleteCourse(courseId);
         setCourses(courses.filter((course) => course._id !== courseId));
     };
-    const updateCourse = () => {
+    const updateCourse = async () => {
+        const status2 = await courseClient.updateCourse(course);
         setCourses(
             courses.map((c) => {
                 if (c._id === course._id) {
