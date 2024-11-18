bc0f32bdb64b71c9893d032d318c0a9a4c4a17c6
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-11-17 19:37:38 -0500

    a5_4.4.2 Creating New Courses

--- src/Kanbas/Account/Session.tsx
+++ src/Kanbas/Account/Session.tsx
@@ -20,5 +20,6 @@ export default function Session({ children }: { children: any }) {
     if (!pending) {
         return children;
     }
+    
 }
 
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -33,8 +33,9 @@ export default function Kanbas() {
         _id: "1234", name: "New Course", number: "New Number",
         startDate: "2023-09-10", endDate: "2023-12-15", description: "New Description",
     });
-    const addNewCourse = () => {
-        setCourses([...courses, { ...course, _id: new Date().getTime().toString() }]);
+    const addNewCourse = async () => {
+        const newCourse = await userClient.createCourse(course);
+        setCourses([...courses, { ...course, newCourse }]);
     };
     const deleteCourse = (courseId: any) => {
         setCourses(courses.filter((course) => course._id !== courseId));
