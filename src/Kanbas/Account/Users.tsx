3bd983a20ec2a8b77f28244f6e5fb46aa87dcb53
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-11-26 22:05:47 -0500

    a6_3.6.7 Creating New Documents in MongoDB with Mongoose

--- src/Kanbas/Account/Users.tsx
+++ src/Kanbas/Account/Users.tsx
@@ -2,11 +2,25 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import PeopleTable from "../Courses/People/Table";
 import * as client from "./client";
+import { FaPlus } from "react-icons/fa";
 export default function Users() {
     const [users, setUsers] = useState<any[]>([]);
     const { uid } = useParams();
     const [role, setRole] = useState("");
     const [name, setName] = useState("");
+    const createUser = async () => {
+        const user = await client.createUser({
+            firstName: "New",
+            lastName: `User${users.length + 1}`,
+            username: `newuser${Date.now()}`,
+            password: "password123",
+            email: `email${users.length + 1}@neu.edu`,
+            section: "S101",
+            role: "STUDENT",
+        });
+        setUsers([...users, user]);
+    };
+
     const filterUsersByName = async (name: string) => {
         setName(name);
         if (name) {
@@ -38,6 +52,10 @@ export default function Users() {
 
     return (
         <div>
+            <button onClick={createUser} className="float-end btn btn-danger wd-add-people">
+                <FaPlus className="me-2" />
+                Users
+            </button>
             <h3>Users</h3>
             <input onChange={(e) => filterUsersByName(e.target.value)} placeholder="Search people"
                 className="form-control float-start w-25 me-2 wd-filter-by-name" />
--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -64,6 +64,12 @@ export const deleteUser = async (userId: string) => {
     return response.data;
 };
 
+export const createUser = async (user: any) => {
+    const response = await axios.post(`${USERS_API}`, user);
+    return response.data;
+};
+
+
 
 
 
