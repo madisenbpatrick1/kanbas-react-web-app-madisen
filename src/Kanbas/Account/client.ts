87e1aa8099d70f898473d1fa21c44c17fd6ba7dc
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-11-26 21:28:56 -0500

    a6_3.6.3 Retrieving Documents by Predicate from MongoDB with Mongoose

--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -43,6 +43,18 @@ export const findAllUsers = async () => {
     return response.data;
 };
 
+export const findUsersByRole = async (role: string) => {
+    const response = await
+        axios.get(`${USERS_API}?role=${role}`);
+    return response.data;
+};
+
+export const findUsersByPartialName = async (name: string) => {
+    const response = await axios.get(`${USERS_API}?name=${name}`);
+    return response.data;
+};
+
+
 
 
 
