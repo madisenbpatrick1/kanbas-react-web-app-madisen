ce634ea915f0d2b08d5339de399772f224ae29f5
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-12-03 18:17:51 -0500

    a6_5.2.4 Deleting Modules

--- src/Kanbas/Courses/Modules/client.ts
+++ src/Kanbas/Courses/Modules/client.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 const MODULES_API = `${REMOTE_SERVER}/api/modules`;
+const axiosWithCredentials = axios.create({ withCredentials: true });
+
 export const deleteModule = async (moduleId: string) => {
-    const response = await axios.delete(`${MODULES_API}/${moduleId}`);
+    const response = await axiosWithCredentials.delete(`${MODULES_API}/${moduleId}`);
     return response.data;
 };
 export const updateModule = async (module: any) => {
