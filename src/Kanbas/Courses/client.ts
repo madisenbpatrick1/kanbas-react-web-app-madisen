897f5137dfd084290346d3f032e6335e7bf1bc13
madisenbpatrick <madisen.bpatrick@gmail.com>
2024-12-09 16:08:21 -0500

    a6_courses done

--- src/Kanbas/Courses/client.ts
+++ src/Kanbas/Courses/client.ts
@@ -1,18 +1,19 @@
 import axios from "axios";
+const axiosWithCredentials = axios.create({ withCredentials: true });
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 const COURSES_API = `${REMOTE_SERVER}/api/courses`;
 export const fetchAllCourses = async () => {
-    const { data } = await axios.get(COURSES_API);
+    const { data } = await axiosWithCredentials.get(COURSES_API);
     return data;
 };
 
 export const deleteCourse = async (id: string) => {
-    const { data } = await axios.delete(`${COURSES_API}/${id}`);
+    const { data } = await axiosWithCredentials.delete(`${COURSES_API}/${id}`);
     return data;
 };
 
 export const updateCourse = async (course: any) => {
-    const { data } = await axios.put(`${COURSES_API}/${course._id}`, course);
+    const { data } = await axiosWithCredentials.put(`${COURSES_API}/${course._id}`, course);
     return data;
 };
 
@@ -49,14 +50,20 @@ export const findEnrollments = async () => {
     return response.data;
 }
 
-export const enrollUserInCourse = async ( userId: string, courseId: string,) => {
-    const response = await axios.post(`${COURSES_API}/${courseId}/enroll/${userId}` );
+export const enrollUserInCourse = async (userId: string, courseId: string,) => {
+    const response = await axios.post(`${COURSES_API}/${courseId}/enroll/${userId}`);
     return response.data;
 }
 
 
-export const unenrollUserInCourse = async ( userId: string, courseId: string,) => {
-    const response = await axios.post(`${COURSES_API}/${courseId}/unenroll/${userId}` );
+export const unenrollUserInCourse = async (userId: string, courseId: string,) => {
+    const response = await axios.post(`${COURSES_API}/${courseId}/unenroll/${userId}`);
     return response.data;
 }
+
+export const createCourse = async (course: any) => {
+    const { data } = await axiosWithCredentials.post(COURSES_API, course);
+    return data;
+};
+
 
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -35,7 +35,7 @@ export default function Kanbas() {
         startDate: "2023-09-10", endDate: "2023-12-15", description: "New Description",
     });
     const addNewCourse = async () => {
-        const newCourse = await userClient.createCourse(course);
+        const newCourse = await courseClient.createCourse(course);
         setCourses([...courses, { ...course, newCourse }]);
     };
     const deleteCourse = async (courseId: any) => {
